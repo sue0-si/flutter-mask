b188b7c658016c3d0f082044042a0802b3c6dc2f
slee3593 <slee3593@gatech.edu>
2024-01-04 03:00:12 +0900

    feat: 판매 수량 UI, 재고 있는 곳만 표시

--- lib/main.dart
+++ lib/main.dart
@@ -67,21 +67,67 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('마스크 재고 있는 곳: ${stores.length}곳'),
+        title: Text('마스크 재고 있는 곳: ${stores.where((element) => element.remainStat
+        != 'break').length}곳'),
         actions: [
           IconButton(onPressed: fetch, icon: const Icon(Icons.refresh))
         ],
       ),
-      body: isLoading ? loadingWidget()
-      : ListView.builder(
-        itemCount: stores.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(stores[index].name ?? ''),
-            subtitle: Text(stores[index].addr ?? ''),
-          );
-        },
-      ),
+      body: isLoading
+          ? loadingWidget()
+          : ListView(
+              children: stores.where((element) => element.remainStat
+              != 'break').map((e) {
+                return ListTile(
+                title: Text(e.name ?? ''),
+                subtitle: Text(e.addr ?? ''),
+                trailing: _buildRemainStateWidget(e),
+                );
+              }).toList()
+            ),
+    );
+  }
+
+  Widget _buildRemainStateWidget(Store store) {
+    var remainStat = '판매종료';
+    var description = '0';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '> 100';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+    }
+
+    return Column(
+      children: <Widget>[
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
+      ],
     );
   }
 
